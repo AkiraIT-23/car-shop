447e9bc2fddc295acf21b1fa0cf379fa087ee456
AkiraIT-23 <jakshylykit@gmail.com>
2024-01-11 15:29:08 +0600

    feat: creating views, serializers, permissions for app:cars

---
+++ cars/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+
+from .models import Category, Car, CarImage
+
+User = get_user_model()
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', ]
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CarImageSerializer(ModelSerializer):
+    class Meta:
+        model = CarImage
+        fields = '__all__'
+
+
+class CarSerializer(ModelSerializer):
+    author = UserSerializer(read_only=True)
+    images = CarImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Car
+        fields = [
+            'id', 'title', 'birthdate', 'price', 'model', 'category', 'author', 'images',
+        ]
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        files = request.FILES.getlist("images")
+        validated_data["author"] = request.user
+        car = Car.objects.create(**validated_data)
+        for image in files:
+            CarImage.objects.create(
+                car=car,
+                image=image
+            )
+        return car
+
+    def update(self, instance, validated_data):
+        request = self.context.get("request")
+        files = request.FILES.getlist("images")
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+
+        if files:
+            CarImage.objects.filter(car=instance).delete()
+            for image in files:
+                CarImage.objects.create(
+                    car=instance,
+                    image=image
+                )
+        return instance
+
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,23 @@
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from .models import Category, Car
+from .permissions import IsAuthorOrReadOnly
+from .serializers import CategorySerializer, CarSerializer
 
 
+class CategoryAPIView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CarAPIView(ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+
+
+class CarDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [IsAuthorOrReadOnly, ]
